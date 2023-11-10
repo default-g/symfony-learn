e699a6a436982c6b0b78f87ee6b1342071f564d5
Artem Obukhov <arteam_101@mail.ru>
2023-11-10 23:55:48 +0700

    Обработка исключений

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Exception\BookCategoryNotFoundException;
 use App\Service\BookService;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Model\BookListResponse;
+use App\Model\ErrorResponse;
 
 class BookController extends AbstractController
 {
@@ -15,13 +19,22 @@ class BookController extends AbstractController
     {
     }
 
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns books inside category",
+     *     @Model(type=BookListResponse::class)
+     * )
+     *
+     * @OA\Response(
+     *     response=404,
+     *     description="Book category not found",
+     *     @Model(type=ErrorResponse::class)
+     * )
+     */
     #[Route('/api/v1/category/{id}/books', name: 'books')]
     public function booksByCategory(int $id): JsonResponse
     {
-        try {
-            return $this->json($this->bookService->getBooksByCategory($id));
-        } catch (BookCategoryNotFoundException $exception) {
-            throw new HttpException($exception->getCode(), $exception->getMessage());
-        }
+        return $this->json($this->bookService->getBooksByCategory($id));
     }
 }
